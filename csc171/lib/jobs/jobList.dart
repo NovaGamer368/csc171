e16723805709d62259abf70e2db432241ca6cb50
Brennan Phifer <bman5803@gmail.com>
2024-02-26 16:03:30 -0700

    Better api calls

--- csc171/lib/jobs/jobList.dart
+++ csc171/lib/jobs/jobList.dart
@@ -19,7 +19,13 @@ class _JobListState extends State<JobList> {
         if (jsonResponse['Results'] is List) {
           List<Future<Object>> apiJobs = [];
           for (var jobObject in jsonResponse['Results']) {
-            apiJobs.add(getJobDetails(jobObject));
+            if (jobObject['ID'] >= 19 &&
+                jobObject['ID'] <= 40 &&
+                jobObject['ID'] != 26 &&
+                jobObject['ID'] != 29 &&
+                jobObject['ID'] != 36) {
+              apiJobs.add(getJobDetails(jobObject));
+            }
           }
           List<dynamic> resolvedJobs = await Future.wait(apiJobs);
           // print(resolvedJobs);
@@ -37,8 +43,6 @@ class _JobListState extends State<JobList> {
 
   Future<Object> getJobDetails(jobObject) async {
     try {
-      print('CALLING OBJECT');
-      print(jobObject);
       final url = Uri.parse('http://xivapi.com${jobObject['Url']}');
       var response = await http.get(url);
       if (response.statusCode == 200) {
@@ -73,14 +77,75 @@ class _JobListState extends State<JobList> {
           return ListView.builder(
             itemCount: snapshot.data?.length ?? 0,
             itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(snapshot.data?[index]['Name'] ?? 'No Name'),
-                subtitle: Text(
-                    snapshot.data?[index]['Abbreviation'] ?? 'No Abbreviation'),
+              String imageUrl = '${snapshot.data![index]['Name']}';
+              // print(imageUrl.replaceAll(' ', ''));
+              return GestureDetector(
+                onTap: () {
+                  print(snapshot.data![index]);
+                  _extraJobDetails(snapshot.data![index]);
+                },
+                child: Card(
+                  elevation: 3,
+                  color: Theme.of(context).primaryColor,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          snapshot.data![index]['Name'] ?? "No Name",
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          'ID: ${snapshot.data![index]['ID']}',
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        const SizedBox(height: 8),
+                        Image.network(
+                          'http://xivapi.com/cj/companion/${imageUrl.replaceAll(' ', '')}.png',
+                          width: 100,
+                          height: 100,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               );
             },
           );
         }
+      },
+    );
+  }
+
+  void _extraJobDetails(Map<String, dynamic> jobDetails) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(jobDetails['Name']),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Abbreviation: ${jobDetails['Abbreviation']}'),
+              Text('Job Category: ${jobDetails['ClassJobCategory']['Name']}'),
+              // Add more job details here as needed
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text('Close'),
+            ),
+          ],
+        );
       },
     );
   }
